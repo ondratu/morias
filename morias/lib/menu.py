9bedcb101b776a819856e443716ab129a9c10f9f
Ondrej Tuma <mcbig@zeropage.cz>
2015-07-15 11:15:35 +0200

    right menu copy in correct_menu function

--- morias/lib/menu.py
+++ morias/lib/menu.py
@@ -19,14 +19,19 @@ class Item(object):
 #endclass Item
 
 class Menu(object):
-    def __init__(self, label, symbol = None, locale = ''):
+    def __init__(self, label, symbol = None, locale = '', role = None):
         self.label = label
         self.symbol = symbol
         self.locale = locale
+        self.role = role
         self.items = []
 
     def append(self, item):
         if isitem(item) or ismenu(item):
+            self.items.append(item)
+
+    def extend(self, menu):
+        for item in menu:
             self.items.append(item)
 
     def __iter__(self):
@@ -41,6 +46,14 @@ class Menu(object):
     def __rep_r__(self):
         return str(self.__dict__)
 
+    @staticmethod
+    def base(menu):
+        return Menu(menu.label,
+                    menu.symbol,
+                    menu.locale,
+                    menu.role)
+    #enddef
+
 #endclass Menu
 
 def isitem(obj):
@@ -49,8 +62,8 @@ def isitem(obj):
 def ismenu(obj):
     return isinstance(obj, Menu)
 
-def correct_menu(req, menu):
-    new_menu = Menu(menu.label)
+def correct_menu(req, menu, retval = None):
+    new_menu = retval if ismenu(retval) else Menu.base(menu)
     for item in menu:
         if isitem(item) and do_match_right(req, item.rights):
             new_menu.append(item)
