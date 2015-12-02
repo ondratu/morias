307890254a6e6226f77f130a78636acbd5e1e983
Ondrej Tuma <mcbig@zeropage.cz>
2015-12-02 08:08:11 +0100

    better internal server erroro for humans

--- morias/core/errors.py
+++ morias/core/errors.py
@@ -1,5 +1,7 @@
 from poorwsgi import app, state
 
+from traceback import format_exc
+
 from render import generate_page
 
 BAD_LOGIN = 1000
@@ -21,4 +23,16 @@ def precondition_failed(req):
     req.state = state.HTTP_PRECONDITION_FAILED
     req.write(generate_page(req, "error/precondition_failed.html",
                             precondition=req.precondition))
+    return state.DONE
+
+
+@app.http_state(state.HTTP_INTERNAL_SERVER_ERROR)
+def internal_server_error(req):
+    traceback = format_exc()
+    traceback = ''.join(traceback)
+    req.log_error(traceback, state.LOG_ERR)
+
+    req.state = state.HTTP_PRECONDITION_FAILED
+    req.write(generate_page(req, "error/internal_server_error.html",
+                            traceback=traceback.split('\n')))
     return state.DONE
