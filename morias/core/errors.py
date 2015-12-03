c574695b3bf3f24d2af591d19fbbd185e3bd28e1
Ondrej Tuma <mcbig@zeropage.cz>
2015-12-03 18:04:55 +0100

    morias native 403 Forbidden Access error

--- morias/core/errors.py
+++ morias/core/errors.py
@@ -10,6 +10,14 @@ NOT_FOUND = 1002
 SUCCESS = 2000
 
 
+@app.http_state(state.HTTP_FORBIDDEN)
+def forbidden(req):
+    req.state = state.HTTP_FORBIDDEN
+    req.write(generate_page(req, "error/forbidden.html",
+                            request_uri=req.environ['REQUEST_URI']))
+    return state.DONE
+
+
 @app.http_state(state.HTTP_NOT_FOUND)
 def not_found(req):
     req.state = state.HTTP_NOT_FOUND
