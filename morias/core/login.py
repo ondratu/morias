ec192d88968f4572a66ec996da5c0298d30cc234
Ondrej Tuma <mcbig@zeropage.cz>
2016-01-15 17:04:12 +0100

    CSRF protection token update
     * Morias Guest Cookie for anonymous user
     * checking token for explicit uri (not only from referer)
     * create_token function for generation token from uri

--- morias/core/login.py
+++ morias/core/login.py
@@ -80,6 +80,32 @@ def do_check_login(req):
 # enddef
 
 
+def do_check_mgc(req):
+    """Check Morias Guest Cookie.
+
+    Check guest cookie and set req.user_hash. If no cookie was readed, or will
+    be bad. Create new cookie and send it to browser.
+
+    Return value:
+        Returun None, if req.login exist yet. So no guest cookie could be used.
+        Returun True, if cookie will be readed or False if be created.
+    """
+    if getattr(req, 'login'):
+        return None     # req.login exist - user cookie exist
+    cookie = PoorSession(req, SID='MGC')    # Morias Guest Cookie
+
+    if "token" not in cookie.data:
+        req.log_error("Creating new cookie.", state.LOG_INFO)
+        cookie.data["timestamp"] = int(time())
+        cookie.data["token"] = urandom(24)
+        req.user_hash = cookie.data["token"]
+        cookie.header(req, req.headers_out)
+        return False
+
+    req.user_hash = cookie.data["token"]
+    return True
+
+
 def do_check_right(req, right):
     return right in req.login.rights or 'super' in req.login.rights
 
@@ -119,11 +145,14 @@ def do_create_token(req, uri):
                           create_referer(req, uri))
 
 
-def do_check_token(req, token):
+def do_check_token(req, token, uri=None):
     """Check token creates by do_create_token."""
-    if req.referer is None:
+    if req.referer is None and uri is None:
         return False
-    referer = req.referer.split('?')[0]
+    if uri:
+        referer = create_referer(req, uri)
+    else:
+        referer = req.referer.split('?')[0]
     return csrf.check_token(token, req.secret_key, req.user_hash, referer)
 
 
@@ -151,7 +180,7 @@ def match_right(req, rights, redirect_uri=None):
 
 
 def check_referer(req, uri, redirect=None):
-    full_referer = create_referer(uri)
+    full_referer = create_referer(req, uri)
     if not req.referer or full_referer != req.referer.split('?')[0]:
         req.log_error("Referer %s not check" % uri, state.LOG_ALERT)
         if redirect:
@@ -172,9 +201,14 @@ def check_origin(req, redirect=None):
         raise SERVER_RETURN(state.HTTP_PRECONDITION_FAILED)
 
 
-def check_token(req, token, redirect=None):
-    if not do_check_token(req, token):
-        req.log_error("Token %s not check" % token, state.LOG_ALERT)
+def create_token(req):
+    """Create token for req.uri"""
+    return do_create_token(req, req.uri.encode('utf-8'))
+
+
+def check_token(req, token, redirect=None, uri=None):
+    if not do_check_token(req, token, uri):
+        req.log_error("Token {0!r} not check".format(token), state.LOG_ALERT)
         if redirect:
             redirect(req, redirect)
         req.precondition = Object()
